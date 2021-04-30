658537684f8772af143c90af7aff7ce1e2a5fb87
Anthony Rosario <anthonymrosario225@gmail.com>
2021-04-30 11:17:13 -0700

    finalization of slack side of app

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ const app = new App({
 });
 
 
-app.action('check-balance', async({ ack, say, body }) => {
+app.action('check-balance', async({ ack, body, client }) => {
   await ack();
   const checkBalance = await request.get(`${process.env.BACKEND_URL}/quack/check-balance/${body.user.id}`);
-  await say(`Your current balance is ${checkBalance.text} Quacks. :duck:`);
+  await client.chat.postEphemeral({
+    channel: body.channel.id,
+    user: body.user.id,
+    text: `Your current balance is ${checkBalance.text} Quacks. :duck:`
+  });
 });
 
 
@@ -44,7 +48,7 @@ app.action('send-quacks', async({ ack, body, client }) => {
             "type": "header",
             "text": {
               "type": "plain_text",
-              "text": "Welcome to the Quackchain! ",
+              "text": "Welcome to the QuackChain! ",
               "emoji": true
             }
           },
@@ -120,7 +124,7 @@ app.action('send-quacks', async({ ack, body, client }) => {
             "element": {
               "type": "plain_text_input",
               "multiline": true,
-              "action_id": "notes-input"
+              "action_id": "notes-action"
             },
             "label": {
               "type": "plain_text",
@@ -131,8 +135,6 @@ app.action('send-quacks', async({ ack, body, client }) => {
         ]
       }
     });
-
-    console.log(result);
   }
   catch (error) {
     console.error(error);
@@ -140,22 +142,32 @@ app.action('send-quacks', async({ ack, body, client }) => {
 });
 
 
-app.view('send-quacks', async({ ack, body, view }) => {
+app.view('send-quacks', async({ ack, body, view, client }) => {
   await ack();
   const senderId = body.user.id;
   const receiverId = view.state.values['receiver-id']['receiver-action'].selected_users[0];
   const amount = view.state.values['amount-input']['amount-action'].value;
-  // console.log(senderId, receiverId, amount);
-  const sendQuacks = await request.post(`${process.env.BACKEND_URL}/quack/send-quacks`).send({
+  await request.post(`${process.env.BACKEND_URL}/quack/send-quacks`).send({
     senderId,
     receiverId,
     amount
   });
-  console.log(sendQuacks);
+  const notes = view.state.values['notes-input']['notes-action'].value;
+  await client.chat.postMessage({
+    channel: receiverId,
+    text: `<@${senderId}> just sent you ${amount} Quacks! 
+    This transaction is being recorded onto the blockchain and should be available in your account in about a minute! 
+    from <@${senderId}>: ${notes} :duck:`
+  });
+  await client.chat.postEphemeral({
+    channel: senderId,
+    user: senderId, 
+    text: `Transaction has been initiated`
+  });
 });
 
 
-app.action('mint-quacks', async({ ack, say, body, client }) => {
+app.action('mint-quacks', async({ ack, body, client }) => {
   await ack();
   try {
     const result = await client.views.open({
@@ -227,33 +239,41 @@ app.action('mint-quacks', async({ ack, say, body, client }) => {
         ]
       }
     });
-
-    console.log(result);
   }
   catch (error) {
     console.error(error);
   }
-  await say(`Quacks created successfully! :duck:`);
 });
 
 
-app.view('mint-quacks', async({ ack, body, view }) => {
+app.view('mint-quacks', async({ ack, body, view, client }) => {
   await ack();
   const slackId = body.user.id;
   const amount = view.state.values['mint-input']['mint-action'].value;
-  const mintQuacks = await request.post(`${process.env.BACKEND_URL}/quack/mint-quacks`).send({
+  await request.post(`${process.env.BACKEND_URL}/quack/mint-quacks`).send({
     slackId,
     amount
   });
+  const accountList = await request.get(`${process.env.BACKEND_URL}/admin/account-list`);
+  const perCapita = Math.floor((amount/accountList.body.length));
+  for(const account of accountList.body) {
+    await client.chat.postMessage({
+      channel: account,
+      text: `<@${slackId}> has minted ${amount} new Quacks. Each holder has received ${perCapita} Quacks.`
+    });
+  }
 });
 
 
-app.command('/quack', async({ command, ack, say }) => {
+
+app.command('/quack', async({ command, ack, client }) => {
   await ack();
   const totalSupply = await request.get(`${process.env.BACKEND_URL}/admin/total-supply`);
   const total = totalSupply.res.text;
-  await say(
+  await client.chat.postEphemeral(
     {
+    "channel": command.channel_id,
+    "user": command.user_id,
     "blocks": [
       {
         "type": "header",
@@ -308,6 +328,137 @@ app.command('/quack', async({ command, ack, say }) => {
     ]
   })
 });
+
+app.event('app_home_opened', async ({ event, client, context }) => {
+  try {
+      const result = await client.views.publish({
+          user_id: event.user,
+          view: {
+            "type": "home",
+            "blocks": [
+              {
+                "type": "header",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Welcome to QuackChain!"
+                }
+              },
+              {
+                "type": "divider"
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "*What is Quack?*"
+                }
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "Quack is a digital currency built on the the Ethereum ERC-20 standard. It is deployed to an Ethereum testnet and used as a workspace specific currency in Slack. This provides a permanent, distributed record of all transactions."
+                }
+              },
+              {
+                "type": "divider"
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "*QuackChain's Mission*"
+                }
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "plain_text",
+                  "text": "QuackChain will allow members of the workspace to show appreciation for one another by sending tips, use Quacks to show support for proposals and participate in a digital marketplace. This will incentivize acts of appreciation, encourage camaraderie and make Slack more fun and interactive.",
+                  "emoji": true
+                }
+              },
+              {
+                "type": "divider"
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "*How to use it!*"
+                }
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "It's quite simple! To interact with the app, simply type */quack* in any channel you are a member of. You will be presented with an interface that let's you send quacks to team members, check your Quack wallet balance and mint new quacks to the blockchain."
+                }
+              },
+              {
+                "type": "divider"
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "*What is minting?*"
+                }
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Every cryptocurrency has a total supply, this is what gives it its value. Users can mint new Quacks to the blockchain, which adds more currency to the total supply. Upon minting new Quacks the minted amount is distributed evenly amongst all current Quack holders. As a fee, half of the minters portion of the distribution will be burned and removed from the total supply to avoid runaway inflation. Mint with care!! :duck:",
+                  "emoji": true
+                }
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "*Here is an example of the minting process*:"
+                }
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "_Alice would like to mint *1000 new Quacks*. There are currently *10* Quack holders, including Alice. Upon minting Alice will receive *50 Quacks* and Bob, another holder, will receive *100 Quacks*. The total supply will increase by *1000 Quacks*._"
+                }
+              },
+              {
+                "type": "divider"
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "*Learn more about the QuackChain project!*"
+                }
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "<https://github.com/Alchemy-Crypto| Github Project Page>"
+                }
+              },
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "<https://github.com/Alchemy-Crypto/docs/blob/main/README.md| QuackChain Creators>"
+                }
+              }
+            ]
+          }
+      });
+  } catch (err) {
+      console.log(err);
+  }
+});
+
 
 
 (async() => {
