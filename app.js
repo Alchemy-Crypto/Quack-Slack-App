d2493ecccb40972cd488df76f701a99cb381a219
Anthony Rosario <anthonymrosario225@gmail.com>
2021-04-30 12:30:26 -0700

    heroku push

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ app.view('send-quacks', async({ ack, body, view, client }) => {
   const receiverId = view.state.values['receiver-id']['receiver-action'].selected_users[0];
   const amount = view.state.values['amount-input']['amount-action'].value;
   const balance = await request.get(`${process.env.BACKEND_URL}/quack/check-balance/${body.user.id}`);
-  console.log('balance line 151', balance);
+  console.log('balance line 151', balance.text);
   if(balance < amount) {
     await client.chat.postEphemeral({
       channel: senderId,
